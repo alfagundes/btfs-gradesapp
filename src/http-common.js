943c4e7aa126db5f84791d1ab76b4b76312b77ef
alfagundes <alexandre.fagundes@neogrid.com>
2020-09-08 16:08:17 -0300

    http-common

--- src/http-common.js
+++ src/http-common.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 //Define a URL base da origem para consumo do servico
 export default axios.create({
   //baseURL: 'http://localhost:8081/',
-  baseURL: 'https://btfs-gradesapi.herokuapp.com/',
+  baseURL: 'https://btfs-gradesapi.herokuapp.com',
   headers: {
     'Content-type': 'application/json',
   },
